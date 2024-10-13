9fe9487cc21bac0fc6ce49d921ed1c42ce3e1afe
randyren278 <randyren278@gmail.com>
2024-10-13 14:39:44 -0700

    lab 2 changes

--- lab2/takehome/CPSC259_Lab2_Takehome/dna.c
+++ lab2/takehome/CPSC259_Lab2_Takehome/dna.c
@@ -6,13 +6,13 @@
               specified sample.  The formatted DNA
               sequence file is a txt file (threes samples
               are provided in the Resource Files folder).
- Author:			Randy Ren
- Student #s:	56695331
- CWLs:	      rren05
- Date:				October 6th, 2024
+ Author:      Randy Ren
+ Student #s:  56695331
+ CWLs:        rren05
+ Date:        October 6th, 2024
  */
 
- /******************************************************************
+/******************************************************************
   PLEASE EDIT THIS FILE
 
   You need to complete the analyze_segments and calculate_score
@@ -34,41 +34,40 @@
 #include <time.h>
 #include "dna.h"
 
-
 /*
  * Determines if the specified molecules (chars) are a base pair
  * PARAM:     a char
  * PARAM:     a char
  * PRE:       molecule is an alphanumeric ascii char
  * RETURN:    IF nucleotide_1 == nucleotide_2
- *			  THEN 1
+ *            THEN 1
  *            ELSE 0
  */
 int is_base_pair(char nucleotide_1, char nucleotide_2)
 {
-  switch (nucleotide_1)
-  {
-  case 'A': case 'a':
-    if (nucleotide_2 == 'T' || nucleotide_2 == 't')
-      return 1;
-    break;
-  case 'T': case 't':
-    if (nucleotide_2 == 'A' || nucleotide_2 == 'a')
-      return 1;
-    break;
-  case 'C': case 'c':
-    if (nucleotide_2 == 'G' || nucleotide_2 == 'g')
-      return 1;
-    break;
-  case 'G': case 'g':
-    if (nucleotide_2 == 'C' || nucleotide_2 == 'c')
-      return 1;
-    break;
-  default:
+    switch (nucleotide_1)
+    {
+    case 'A': case 'a':
+        if (nucleotide_2 == 'T' || nucleotide_2 == 't')
+            return 1;
+        break;
+    case 'T': case 't':
+        if (nucleotide_2 == 'A' || nucleotide_2 == 'a')
+            return 1;
+        break;
+    case 'C': case 'c':
+        if (nucleotide_2 == 'G' || nucleotide_2 == 'g')
+            return 1;
+        break;
+    case 'G': case 'g':
+        if (nucleotide_2 == 'C' || nucleotide_2 == 'c')
+            return 1;
+        break;
+    default:
+        return 0;
+        break;
+    }
     return 0;
-    break;
-  }
-  return 0;
 }
 
 /*
@@ -83,19 +82,17 @@ int is_base_pair(char nucleotide_1, char nucleotide_2)
  */
 int get_codon_index(char* codon_code)
 {
-  int i;
-  for (i = 0; i < NUMBER_OF_CODONS; ++i) {
+    int i;
+    for (i = 0; i < NUMBER_OF_CODONS; ++i) {
 
-    if (codon_codes[i][0] == codon_code[0] &&
-      codon_codes[i][1] == codon_code[1] &&
-      codon_codes[i][2] == codon_code[2]) {
-      return i;
+        if (codon_codes[i][0] == codon_code[0] &&
+            codon_codes[i][1] == codon_code[1] &&
+            codon_codes[i][2] == codon_code[2]) {
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
-
-
 
 /*
  * Extracts the DNA segment information stored in a correctly formatted file.
@@ -116,99 +113,99 @@ int get_codon_index(char* codon_code)
  */
 int extract_dna(FILE* file_pointer, char** sample_segment, char*** candidate_segments)
 {
-  /* Variables */
-  int  i = 0, j = 0;
-  int  return_value = 0;
-  int  new_line = 0;
-  int  line_length = 0;
-  int  sequence_length = 0;
-  int  number_of_candidates = 0;
-  char character = ' ';
-  char line_buffer[BUFSIZE];
+    /* Variables */
+    int  i = 0, j = 0;
+    int  return_value = 0;
+    int  new_line = 0;
+    int  line_length = 0;
+    int  sequence_length = 0;
+    int  number_of_candidates = 0;
+    char character = ' ';
+    char line_buffer[BUFSIZE];
 
-  /* Moves to the beginning of the file */
-  fseek(file_pointer, 0, SEEK_SET);
+    /* Moves to the beginning of the file */
+    fseek(file_pointer, 0, SEEK_SET);
 
-  /* Here's an easy way to ignore the first line (the header)! */
-  while ((character = fgetc(file_pointer)) != '\n') { ; }
-
-  /* Acquires sample sequence (we know it's in the second line) */
-  while (new_line == 0 && fgets(line_buffer, BUFSIZE, file_pointer)) {
-
-    /* Check if the line ends with a newline character and increments the sample length */
-    if (line_buffer[strlen(line_buffer) - 1] == '\n') {
-      new_line = 1;
-      line_length = strlen(line_buffer) - 1;
-    }
-    else {
-      line_length = strlen(line_buffer);
-      new_line = 0;
-    }
-
-    /* (Re)allocates some space for the (additional) nucleotides */
-    *sample_segment = (char*)realloc(*sample_segment, sizeof(char) * (sequence_length + line_length));
-
-    /* Copies the contents of the line buffer to the end of the dynamically (re)allocated memory */
-    for (i = 0; i < line_length; ++i) {
-      *(*sample_segment + sequence_length + i) = line_buffer[i];
-    }
-    sequence_length += line_length;
-  }
-
-  /* Adds terminating null character to sample, so we can treat it as a null-terminated string */
-  *sample_segment = (char*)realloc(*sample_segment, sizeof(char) * (sequence_length + 1));
-  *(*sample_segment + sequence_length) = '\0';
-
-  /* Acquires number of candidate sequences (from the third line of the file) */
-  fgets(line_buffer, BUFSIZE, file_pointer);
-  return_value = sscanf(line_buffer, "%d", &number_of_candidates);
-
-  /* Allocates pointers for correct number of candidate sequences */
-  *candidate_segments = (char**)malloc(sizeof(char*) * number_of_candidates);
-  for (i = 0; i < number_of_candidates; ++i) {
-    *(*candidate_segments + i) = NULL; /* What happens if we omit this line? */
-  }
-
-  /* Copies each candidate sequence, in order, from the file to the memory we just
-     dynamically allocated */
-  for (i = 0; i < number_of_candidates; ++i) {
-
-    sequence_length = 0; /* Resets the variable */
-    new_line = 0; /* Resets the variable */
-
-    /* Here's an easy way to ignore each candidate sequence's header */
+    /* Here's an easy way to ignore the first line (the header)! */
     while ((character = fgetc(file_pointer)) != '\n') { ; }
 
-    /* Acquires candidate sequence */
+    /* Acquires sample sequence (we know it's in the second line) */
     while (new_line == 0 && fgets(line_buffer, BUFSIZE, file_pointer)) {
 
-      /* Check if the line ends with a newline character and sets length */
-      if (line_buffer[strlen(line_buffer) - 1] == '\n') {
-        new_line = 1;
-        line_length = strlen(line_buffer) - 1;
-      }
-      else {
-        line_length = strlen(line_buffer);
-        new_line = 0;
-      }
+        /* Check if the line ends with a newline character and increments the sample length */
+        if (line_buffer[strlen(line_buffer) - 1] == '\n') {
+            new_line = 1;
+            line_length = strlen(line_buffer) - 1;
+        }
+        else {
+            line_length = strlen(line_buffer);
+            new_line = 0;
+        }
 
-      /* (Re)allocates some space for the (additional) nucleotides */
-      *(*candidate_segments + i) = (char*)realloc(*(*candidate_segments + i), sizeof(char) * (sequence_length + line_length));
+        /* (Re)allocates some space for the (additional) nucleotides */
+        *sample_segment = (char*)realloc(*sample_segment, sizeof(char) * (sequence_length + line_length));
 
-      /* Copies the contents of the line buffer to the end of the dynamically (re)allocated memory */
-      for (j = 0; j < line_length; ++j) {
-        *(*(*candidate_segments + i) + sequence_length + j) = line_buffer[j];
-      }
-      sequence_length += line_length;
+        /* Copies the contents of the line buffer to the end of the dynamically (re)allocated memory */
+        for (i = 0; i < line_length; ++i) {
+            *(*sample_segment + sequence_length + i) = line_buffer[i];
+        }
+        sequence_length += line_length;
     }
 
-    /* Adds terminating null character to candidate, so we can treat it as a null-terminated string */
-    *(*candidate_segments + i) = (char*)realloc(*(*candidate_segments + i), sizeof(char) * (sequence_length + 1));
-    *(*(*candidate_segments + i) + sequence_length) = '\0';
-    new_line = 0;
-  }
+    /* Adds terminating null character to sample, so we can treat it as a null-terminated string */
+    *sample_segment = (char*)realloc(*sample_segment, sizeof(char) * (sequence_length + 1));
+    *(*sample_segment + sequence_length) = '\0';
 
-  return number_of_candidates;
+    /* Acquires number of candidate sequences (from the third line of the file) */
+    fgets(line_buffer, BUFSIZE, file_pointer);
+    return_value = sscanf(line_buffer, "%d", &number_of_candidates);
+
+    /* Allocates pointers for correct number of candidate sequences */
+    *candidate_segments = (char**)malloc(sizeof(char*) * number_of_candidates);
+    for (i = 0; i < number_of_candidates; ++i) {
+        *(*candidate_segments + i) = NULL; /* What happens if we omit this line? */
+    }
+
+    /* Copies each candidate sequence, in order, from the file to the memory we just
+       dynamically allocated */
+    for (i = 0; i < number_of_candidates; ++i) {
+
+        sequence_length = 0; /* Resets the variable */
+        new_line = 0; /* Resets the variable */
+
+        /* Here's an easy way to ignore each candidate sequence's header */
+        while ((character = fgetc(file_pointer)) != '\n') { ; }
+
+        /* Acquires candidate sequence */
+        while (new_line == 0 && fgets(line_buffer, BUFSIZE, file_pointer)) {
+
+            /* Check if the line ends with a newline character and sets length */
+            if (line_buffer[strlen(line_buffer) - 1] == '\n') {
+                new_line = 1;
+                line_length = strlen(line_buffer) - 1;
+            }
+            else {
+                line_length = strlen(line_buffer);
+                new_line = 0;
+            }
+
+            /* (Re)allocates some space for the (additional) nucleotides */
+            *(*candidate_segments + i) = (char*)realloc(*(*candidate_segments + i), sizeof(char) * (sequence_length + line_length));
+
+            /* Copies the contents of the line buffer to the end of the dynamically (re)allocated memory */
+            for (j = 0; j < line_length; ++j) {
+                *(*(*candidate_segments + i) + sequence_length + j) = line_buffer[j];
+            }
+            sequence_length += line_length;
+        }
+
+        /* Adds terminating null character to candidate, so we can treat it as a null-terminated string */
+        *(*candidate_segments + i) = (char*)realloc(*(*candidate_segments + i), sizeof(char) * (sequence_length + 1));
+        *(*(*candidate_segments + i) + sequence_length) = '\0';
+        new_line = 0;
+    }
+
+    return number_of_candidates;
 }
 
 /*
@@ -292,8 +289,13 @@ void analyze_segments(char* sample_segment, char** candidate_segments, int numbe
             strcat(output_string, outputline_buffer);
         }
     }
-}
 
+    // Remove the last newline character if any
+    int len = strlen(output_string);
+    if (output_string[len - 1] == '\n') {
+        output_string[len - 1] = '\0';
+    }
+}
 
 /*
  * Compares the sample segment and the candidate segment and calculates a
@@ -307,22 +309,22 @@ void analyze_segments(char* sample_segment, char** candidate_segments, int numbe
  *    b) else if the 2 codons are different but specify the same amino acid, add 5 to the score
  *    c) else if the two codons are different and do not specify the same amino acid, then
  *       examine the 3 nucleotides in the codon separately:
- *		 For each of the 3 nucleotides:
- *		i)   If the two nucleotides are the same, add 2 to the score
- *      ii)  If the 2 nucleotides belong to a matching base pair (A and T, or C and G), add 1
- *      iii) Otherwise, add zero to the score (e.g., do nothing)
+ *       For each of the 3 nucleotides:
+ *       i)   If the two nucleotides are the same, add 2 to the score
+ *       ii)  If the 2 nucleotides belong to a matching base pair (A and T, or C and G), add 1
+ *       iii) Otherwise, add zero to the score (e.g., do nothing)
  * 5. Store the result
  * 6. Now skip the first codon in the candidate, e.g., shift the sample by 1 codon,
  *    and check the score of this new alignment.  Compare this score to the score
  *    you calculated before shifting, and store the higher score.  Keep shifting
  *    the sample deeper into the candidate by one codon at a time, checking the score, and
- *    storing ONLY the top score, until itno longer fits. Once the sample (ignoring the
+ *    storing ONLY the top score, until it no longer fits. Once the sample (ignoring the
  *    trailing nucleotides) extends beyond the length of the candidate we stop.
  * PARAM:     pointer to the sample segment
  * PARAM:     pointer to the candidate segment
  * PRE:       pointers are valid pointers to dynamically allocated and populated memory
  * POST:      NULL
- * RETURN:    An int score representing to degree to which the two segments match.
+ * RETURN:    An int score representing the degree to which the two segments match.
  */
 int calculate_score(char* sample_segment, char* candidate_segment) {
     int score = 0, temp_score = 0;
@@ -332,7 +334,7 @@ int calculate_score(char* sample_segment, char* candidate_segment) {
     int i, j, k;
 
     // Loop through candidate segment, sliding the sample over it
-    for (i = 0; i <= candidate_length - sample_length; i += 3) {
+    for (i = 0; i <= candidate_length - (sample_codons * 3); i++) {
         temp_score = 0;
 
         // Compare codon by codon
